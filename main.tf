502d8bbc272252afbe613d78dab69c7f27a4c5f0
kytan2992 <kytan2992@gmail.com>
2025-03-21 15:54:53 +0800

    push terraform code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_instance" {
   ami                         = data.aws_ami.ami_linux.id
   instance_type               = "t2.micro"
-  subnet_id                   = data.aws_vpc.vpc.public_subnets[0]
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   key_name                    = var.keypair
   vpc_security_group_ids      = [data.aws_security_group.sg.id]
